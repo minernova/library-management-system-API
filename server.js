cb5cbef059c673eea00b80f882cfb009bf8c5199
Bharti Patel <patelshree40@gmail.com>
2023-09-29 20:59:56 +0530

    routes created

--- server.js
+++ server.js
@@ -3,20 +3,21 @@ const mongoose=require('mongoose')
 const app=express()
 app.use(express.static(__dirname + '/public'));
 
+const profileRouter=require('./routes/profile')
+const bookRouter=require('./routes/books')
+
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser:true,useUnifiedTopology:true})
 
 
 app.set('view engine','ejs')
+
 app.get('/',(req,res)=>{
     res.render('index')
 })
-app.get('/books',(req,res)=>{
-    res.render('books')
-})
-app.get('/profile',(req,res)=>{
-    res.render('profile')
-})
+
+app.use('/profile',profileRouter)
+app.use('/books',bookRouter)
 
 app.listen(process.env.PORT||3000);
