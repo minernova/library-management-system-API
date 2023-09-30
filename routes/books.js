a69430453f5fff2afd23cdcbe8abb0443fda6116
Bharti Patel <patelshree40@gmail.com>
2023-09-30 08:35:48 +0530

    searching funtionality added

--- routes/books.js
+++ routes/books.js
@@ -1,30 +1,14 @@
 const router = require('express').Router();
-//const Book = require('../models/Book'); // Import your Book model
-
-router.get("/",(req,res)=>{
-    res.render("books")
-})
+const Book = require('../models/Book'); // Import your Book model
 
 
-router.get("/", (req, res) => {
-    const searchQuery = req.query.search || ''; 
-    filterBooksByTitle(searchQuery, res);
-  });
-  
-  // Function to filter books by title
-  function filterBooksByTitle(searchQuery, res) {
-    // Here, you would perform a database query to find books based on the search query.
-    // For the sake of this example, let's assume you have a "Book" model and you want
-    // to find books whose title contains the search query.
-  
-    // Replace this with your actual database query
-    Book.find({ title: { $regex: new RegExp(searchQuery, 'i') } })
-      .then((books) => {
-        res.render("books", { books }); // Render the books view with filtered books
-      })
-      .catch((err) => {
-        console.error("Error filtering books:", err);
-        res.status(500).send("An error occurred while filtering books.");
-      });
-  }
+const getBooks = async (req, res) => {
+  const myBooks = await Book.find(req.query);
+   
+    res.status(200).json({ myBooks }); // Send the retrieved books
+};
+
+
+router.get('/', getBooks); // Handle GET requests to /books (Retrieve all books)
+
 module.exports = router;
--- server.js
+++ server.js
@@ -27,3 +27,5 @@ const start = async()=>{
         console.log(error);
     }
 }
+
+start();
