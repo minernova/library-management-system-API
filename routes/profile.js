8e17f4f00b2fc44a80668250f9ba6ffe2923fae3
minernova <34124239+minernova@users.noreply.github.com>
2023-10-09 04:17:24 +0530

    add fetch student

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,12 @@
+const Student = require('../models/Student'); // Import your Book model
 const router = require('express').Router();
 
-router.get("/",(req,res)=>{
-    res.render("profile")
+
+router.get("/", async (req, res) => {
+    var search = req.query.search || ''
+    var student = await Student.find({})
+    console.log(student)
+    res.render("profile", { student: student})
 })
 
 module.exports = router;
